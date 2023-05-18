5a187d592162271a2d04c8a8c9cfd66fa944edf5
jkpark-vcanus <jkpark@vcanus.com>
2023-05-18 14:09:50 +0900

    time

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,30 @@
-export default function hello(name: string): string {
-  return `hello ${name}`;
+// time
+export function getKoreaTime(): Date {
+  const now = new Date();
+  const utcNow = now.getTime() + now.getTimezoneOffset() * 60 * 1000;
+  const koreaTimeDiff = 9 * 60 * 60 * 1000;
+  return new Date(utcNow + koreaTimeDiff);
 }
 
-export { add } from './calc';
+export function dateToString(date: Date): string {
+  return (
+    `${date.getFullYear()}-` +
+    `${date.getMonth() + 1}`.padStart(2, '0') +
+    `-` +
+    `${date.getDate()}`.padStart(2, '0')
+  );
+}
+
+export function addMonth(date: Date, add: number): Date {
+  date.setMonth(date.getMonth() + add);
+  return date;
+}
+
+export function addDate(date: Date, add: number): Date {
+  date.setDate(date.getDate() + add);
+  return date;
+}
+
+export function diffDate(start: Date, end: Date): number {
+  return (end.getTime() - start.getTime()) / 86400000;
+}
